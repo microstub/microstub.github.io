7809a580c6c5e0b3c31d59905591003db22f1ebf
Lunar Journal <md.node.0@gmail.com>
2024-06-17 12:16:21 +0200

    Add files via upload

---
+++ assets/js/clipboard.js
@@ -0,0 +1,27 @@
+const codeElements = document.querySelectorAll('code');
+
+const createCopyButton = (codeElement) => {
+    const img = document.createElement('img');
+    img.classList.add('copy-code');
+    img.src = '/assets/copy.svg';
+
+    img.addEventListener('click', () => {
+        const codeText = codeElement.innerText;
+        navigator.clipboard.writeText(codeText).then(() => {
+            img.style.opacity = '0.7';
+        }).catch((err) => {
+            console.error('Failed to copy text: ', err);
+        });
+    });
+
+    return img;
+};
+
+// Log each <code> element to the console
+codeElements.forEach((codeElement) => {
+    const parentElement = codeElement.parentElement;
+    if (parentElement && parentElement.classList.contains('highlight')) {
+        const copyButton = createCopyButton(codeElement);
+        codeElement.insertBefore(copyButton, codeElement.firstChild);
+    }
+});
